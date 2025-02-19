21f7a710cef43d630559d1998fee73b1bb290411
Walter Pereira Cruz <wpereira04@vt.edu>
2025-02-19 13:59:07 -0500

    next attempt

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+////////////////////////////////////////////////////////
+//
+// ECE 3574, A4, Walter Pereira Cruz
+// File name: main.cpp
+// Description: Contains nothing used to ensure proper
+// building.
+//				
+// Date:        02/19/2025
+//
+#include "hybridCar.h"
+
+int main() {
+
+	return 0;
+}
